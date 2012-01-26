1317a78fab62bbbf7d79f4d67b0a5871ccbeb21a
Gavrie Philipson <gavriep@il.ibm.com>
2012-01-26 20:50:04 +0200

    empty

--- pywiki/tests/test_pages.py
+++
@@ -1,9 +0,0 @@
-from .. import pages
-
-def test_page_get():
-    mytitle = "A Test Page"
-    mycontent = "Some page content..."
-
-    pages.set_page(mytitle, mycontent)
-    assert pages.get_page(mytitle) == mycontent
-
