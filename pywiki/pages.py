9b95ca8c9a50c6e75c83dc3561e27e0515a3c27d
Gavrie Philipson <gavriep@il.ibm.com>
2012-01-30 15:10:02 +0200

    iter

--- pywiki/pages.py
+++ pywiki/pages.py
@@ -3,15 +3,37 @@ class Page(object):
         self.title = title
         self.content = content
 
+    def __repr__(self):
+        return "<Page '{}'>".format(self.title)
+
+
+class PageIterator(object):
+    def __init__(self, pages):
+        self._pages = pages
+        self._keys = list(pages.keys())
+
+    def next(self):
+        try:
+            return self._pages[self._keys.pop()]
+        except IndexError:
+            raise StopIteration
+
+
 class PageCollection(object):
     def __init__(self):
-        self._pages = {}
+        self._pages = { 
+            "first":  Page("first", "foo"),
+            "second": Page("second", "bar"),
+        }
 
     def __setitem__(self, title, content):
         self._pages[title] = Page(title, content)
 
     def get(self, title):
         return self._pages[title].content
+
+    def __iter__(self):
+        return PageIterator(self._pages)
 
 #######################################
 
