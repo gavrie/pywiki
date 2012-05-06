88cbd80f5ba2afed640b03bdc08aed544d9adfdc
Gavrie Philipson <gavriep@il.ibm.com>
2012-05-06 11:28:03 +0300

    Cleaned up test

--- pywiki/tests/test_pages.py
+++ pywiki/tests/test_pages.py
@@ -1,6 +1,6 @@
 from .. import pages
 
-def test_page_get():
+def test_setting_a_page_and_then_getting_it_returns_the_correct_content():
     mytitle = "A Test Page"
     mycontent = "Some page content..."
 
