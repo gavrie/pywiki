afc684869b1303f397d122ee21ff5df7530dcb86
Gavrie Philipson <gavriep@il.ibm.com>
2012-05-07 14:34:45 +0300

    more tests

--- pywiki/tests/test_pages.py
+++ pywiki/tests/test_pages.py
@@ -1,5 +1,7 @@
 from .. import pages
 
+import pytest
+
 def test_setting_a_page_and_then_getting_it_returns_the_correct_content():
     mytitle = "A Test Page"
     mycontent = "Some page content..."
@@ -7,3 +9,12 @@ def test_setting_a_page_and_then_getting_it_returns_the_correct_content():
     pages.set_page(mytitle, mycontent)
     assert pages.get_page(mytitle) == mycontent
 
+def test_updating():
+    mytitle = "A Test Page"
+    mycontent2 = "Some page content (updated)..."
+    pages.set_page(mytitle, mycontent2)
+    assert pages.get_page(mytitle) == mycontent2
+
+def test_getting_a_nonexistent_page_fails():
+    with pytest.raises(KeyError):
+        assert pages.get_page("Foo Bar")
